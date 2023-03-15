b34e2e9b178c94049f9484c4f73d9eb50fc697e8
TomasViveiros <al77349@alunos.utad.pt>
2023-03-15 11:46:51 +0000

    feito2

--- Al77349.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Al77349
-{
-	public static void Apresentacao()
-	{
-		Console.WriteLine("al77349");
-	}
-}
---
+++ projeto02/Al77349.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Al77349
+{
+    public static void Apresentacao()
+    {
+        Console.WriteLine("al77349");
+
+    }
+}
